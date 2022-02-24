ce7623ef618f2ba50952ad15192b452b9e7a4591
Josue <josuepereztorr@gmail.com>
2022-02-23 20:11:55 -0600

    Updated MissingNameException and InvalidBirthdayException return message

--- src/edu/wctc/InvalidBirthdayException.java
+++ src/edu/wctc/InvalidBirthdayException.java
@@ -2,6 +2,6 @@ package edu.wctc;
 
 public class InvalidBirthdayException extends Exception {
     public InvalidBirthdayException(int year, int month, int day) {
-        super(String.format("Invalid Birthday : the given birthday is invalid, %s-%s-%s",year, month, day));
+        super(String.format("Invalid Birthday : '%s-%s-%s' us an invalid birthdate",year, month, day));
     }
 }
