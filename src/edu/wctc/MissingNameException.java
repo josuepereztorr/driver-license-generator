ba077e7721324ccbd35624b647055a6904dfcd08
Josue <josuepereztorr@gmail.com>
2022-02-23 20:04:39 -0600

    Created MissingNameException that extends Exception class.

---
+++ src/edu/wctc/MissingNameException.java
@@ -0,0 +1,7 @@
+package edu.wctc;
+
+public class MissingNameException extends Exception {
+    public MissingNameException(String nameType) {
+        super(String.format("Missing Name : %s cannot be blank or missing", nameType));
+    }
+}
