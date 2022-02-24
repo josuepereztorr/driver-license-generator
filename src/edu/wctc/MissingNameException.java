f078112f41e0670d4a4ce7709c1ac32ae63445a8
Josue <josuepereztorr@gmail.com>
2022-02-23 20:11:04 -0600

    Updated MissingNameException return message

--- src/edu/wctc/MissingNameException.java
+++ src/edu/wctc/MissingNameException.java
@@ -2,6 +2,6 @@ package edu.wctc;
 
 public class MissingNameException extends Exception {
     public MissingNameException(String nameType) {
-        super(String.format("Missing Name : %s cannot be blank or missing", nameType));
+        super(String.format("Missing Name : '%s' cannot be blank or missing", nameType));
     }
 }
